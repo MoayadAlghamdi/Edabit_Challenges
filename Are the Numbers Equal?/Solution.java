4309114965c1baf47fef9278a56b452d6f2b7da6
MDFCIT <mdfcit.16@gmail.com>
2020-07-27 23:32:45 +0300

    Add files via upload

---
+++ Are the Numbers Equal?/Solution.java
@@ -0,0 +1,15 @@
+package edabit_challenges;
+
+public class Solution {
+
+    public static boolean isEqual(int num1, int num2) {
+        // For check of the first number is equal to the second number. 
+        if (num1 == num2) {
+            // If it's equal  , return true 
+            return true;
+        } else {
+            // If it's not equal , return false
+            return false;
+        }
+    }
+}
