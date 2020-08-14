10fb626c8f9cf311e7d97dd53a8d1607955777b9
MDFCIT <mdfcit.16@gmail.com>
2020-08-14 23:57:57 +0300

    Update Solution.java

--- Persistence/Solution.java
+++ Persistence/Solution.java
@@ -1,14 +1,38 @@
 package edabit_challenges;
 
 public class Solution {
-
-   
-    public static int triangle(int n) {
-        int sum = 0;
-        for (int i = 1; i <= n; i++) {
-            sum += i;
+    public static int additivePersistence(int n) {
+        String str = String.valueOf(n);
+        int counter = 0;
+        while (true) {
+            int sum = 0;
+            if (str.length() > 1) {
+                for (char letter : str.toCharArray()) {
+                    sum += Integer.parseInt(letter + "");
+                }
+                str = String.valueOf(sum);
+                counter++;
+            } else {
+                return counter;
+            }
         }
-        return sum ;
+    }
+    public static long multiplicativePersistence(long n) {
+        String str = String.valueOf(n);
+        long counter = 0;
+        while (true) {
+            long sum = 1;
+            if (str.length() > 1) {
+                for (char letter : str.toCharArray()) {
+                    sum *= Long.parseLong(letter + "");
+                }
+                str = String.valueOf(sum);
+                counter++;
+            } else {
+                return counter;
+            }
+        }
+
     }
 }
 
